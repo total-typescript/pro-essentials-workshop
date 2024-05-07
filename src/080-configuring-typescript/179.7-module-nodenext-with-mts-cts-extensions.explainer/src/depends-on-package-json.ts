aaeaa6f55abb47e5d14c8b70f2e5463abeef1205
Matt Pocock <mattpocockvoice@gmail.com>
2024-05-07 11:16:18 +0100

    Fixed 179.7

--- src/080-configuring-typescript/179.7-module-nodenext-with-mts-cts-extensions.explainer/src/depends-on-package-json.ts
+++ src/080-configuring-typescript/179.7-module-nodenext-with-mts-cts-extensions.explainer/src/depends-on-package-json.ts
@@ -1,6 +1,6 @@
 // moduleResolution: Bundler doesn't require '.js' extensions!
 // import { example } from "./example";
 
-import { example } from "./example.cjs";
+import { example } from "./commonjs.cjs";
 
 example();
---
+++ src/080-configuring-typescript/179.7-module-nodenext-with-mts-cts-extensions.explainer/src/esmodule.mts
@@ -0,0 +1,6 @@
+// moduleResolution: Bundler doesn't require '.js' extensions!
+// import { example } from "./example";
+
+import { example } from "./commonjs.cjs";
+
+example();
